1f0c5eaca8979cfdb099c3194421f7b7d6773cfd
Keiron Harper <kjharper91@gmail.com>
2022-10-05 14:24:06 +0100

    netlify

---
+++ dist/server.js
@@ -0,0 +1,28 @@
+const tweet = require('./routes/tweets');
+const trends = require('./routes/trends');
+const news = require('./routes/news');
+const express = require("express");
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+var bodyParser = require('body-parser')
+const app = express();
+const serverless = require('serverless-http');
+var cors = require('cors')
+
+
+app.use(cors())
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(path.resolve(__dirname, './build')));
+
+app.use("/.netlify/functions/server/api", tweet);
+app.use("/.netlify/functions/server/api", trends);
+app.use("/.netlify/functions/server/api", news);
+
+app.use('/.netlify/functions/server', router);
+
+module.exports = app
+module.exports.handler = serverless(app);
