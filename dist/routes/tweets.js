33d9e6c65bcbc97b9c4121e159dace02afd55ce3
Keiron Harper <kjharper91@gmail.com>
2022-10-05 18:29:41 +0100

    i

--- dist/routes/tweets.js
+++ dist/routes/tweets.js
@@ -1,11 +1,15 @@
-let {tweet} = require('../config/Twitter-config');
+let { tweet } = require('../config/Twitter-config');
 let express = require('express');
 let router = express.Router();
 
-router.post('/tweets',(req,res) => {
+router.post('/tweets', (req, res) => {
 
-  tweet('tweets', {'ids': req.body}).then((tweet) => {
-    return res.json(tweet);
+  tweet('tweets', { 'ids': req.body }).then((tweet) => {
+    return res.json({
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      statusCode: 200,
+      body: JSON.stringify(tweet)
+    });
   });
 })
 
